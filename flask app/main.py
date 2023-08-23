fc502339119bc4f90e7f9b075d38c55c5eba4485
GianlucaTurra <gianlucaturra1@gmail.com>
2023-08-23 10:55:49 +0200

    Insert flask app into folder

---
+++ flask app/main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tournaments.db'
+db = SQLAlchemy(app)
+
+
+def create_tournament_table_class(table_name: str):
+    class TournamentTable(db.Model):
+        sql_table_name = table_name.lower().replace(' ', '_')
+        __tablename__ = sql_table_name
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        player = db.Column(db.String, unique=True, nullable=False)
+        points = db.Column(db.Integer, nullable=True)
+        matches_won = db.Column(db.Integer, nullable=True)
+        matches_drawn = db.Column(db.Integer, nullable=True)
+        games_won = db.Column(db.Integer, nullable=True)
+    return TournamentTable
+
+
+app.app_context().push()
+db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/tournaments', methods=['POST'])
+def create_tournament():
+    try:
+        tournament_name_json = request.get_json()
+        create_tournament_table_class(table_name=tournament_name)
+        db.create_all()
+        response_data = {'message': 'Data received successfully.'}
+        return jsonify(response_data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@app.route('/players', methods=['GET', 'POST'])
+def add_player():
+    player_name = request.form.get('player')
+    db.session.add(player_name)
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
