7d44067664b67d638b46735838721c15d62c6833
GianlucaTurra <gianlucaturra1@gmail.com>
2023-09-01 14:50:26 +0200

    Created models.py with table model Added new functions for api calls Tried and failed to end the mess these files are

--- flask app/main.py
+++ flask app/main.py
@@ -1,47 +1,11 @@
-import json
 import flask_functions
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
+from models import Player, Tournament, Match, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tournaments.db'
-db = SQLAlchemy(app)
 
-
-class Players(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, unique=False, nullable=False)
-
-
-# Tournament with data for each one
-class Tournaments(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    date = db.Column(db.String, unique=False, nullable=False)
-
-
-# Rounds of each tournament (probably not even need, maybe)
-class Rounds(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    tournament = db.Column(db.String, unique=False, nullable=False)
-    round_number = db.Column(db.Integer, unique=False, nullable=False)
-    player1 = db.Column(db.String, unique=False, nullable=False)
-    player2 = db.Column(db.String, unique=False, nullable=False)
-    player1_result = db.Column(db.Integer, unique=False, nullable=False)
-    player2_result = db.Column(db.Integer, unique=False, nullable=False)
-
-
-# Player with tournament and their result
-class PlayersAndTournaments(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    tournament = db.Column(db.String, unique=False, nullable=False)
-    player = db.Column(db.String, unique=True, nullable=False)
-    points = db.Column(db.Integer, nullable=True)
-    matches_won = db.Column(db.Integer, nullable=True)
-    matches_drawn = db.Column(db.Integer, nullable=True)
-    games_won = db.Column(db.Integer, nullable=True)
-
-
+db.init_app(app)
 app.app_context().push()
 db.create_all()
 
@@ -53,31 +17,34 @@ def home():
     return jsonify(response_data), 200
 
 
-# Populate PlayersAndTournament at the tournament start (url to be updated)
-@app.route('/tournaments/start', methods=['POST'])
+# Create tournament row in the tournament db table
+@app.route('/tournaments/start_new', methods=['POST'])
 def start_tournament():
     try:
-        response_data = flask_functions.add_players_to_tournament_db(db=db, db_table=PlayersAndTournaments)
+        response_data = flask_functions.start_tournament(db=db, tournament_table=Tournament, player_table=Player)
         return jsonify(response_data), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-# Create tournament row in the tournament db table
-@app.route('/tournaments', methods=['POST'])
-def create_tournaments():
+# Get list of saved players
+@app.route('/players/saved', methods=['GET'])
+def saved_players():
     try:
-        response_data = flask_functions.create_tournament(db=db, db_table=Tournaments)
-        return jsonify(response_data), 201
+        players = flask_functions.get_saved_players(db_table=Player)
+        return jsonify(players)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+# The following routes are not yet implemented or actually useful
 
 
 # Save player into the player db table
 @app.route('/players/add', methods=['POST'])
 def add_players():
     try:
-        response_data = flask_functions.add_players_to_db(db=db, db_table=Players)
+        response_data = flask_functions.add_players_to_db(db=db, db_table=Player)
         return jsonify(response_data), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -87,25 +54,32 @@ def add_players():
 @app.route('/rounds', methods=['POST'])
 def add_rounds():
     try:
-        response_data = flask_functions.add_rounds_to_db(db=db, db_table=Rounds)
+        response_data = flask_functions.add_rounds_to_db(db=db, db_table=Match)
         return jsonify(response_data), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-# Probably a pile of garbage
-@app.route('/delete_tournament', methods=['DELETE'])
-def delete_tournament():
-    try:
-        tournament = json.loads(request.get_json())
-        tournament_name = tournament['name']
-        db.session(f'DROP TABLE {tournament_name}')
-        db.session.commit()
-        response_data = {'message': f'{tournament_name} table deleted'}
-        return jsonify(response_data), 200
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 400
+# Get tournament current results
+@app.route('/tournaments/<int:tournament_id>', methods=['GET'])
+def get_tournament_name_and_players(tournament_id):
+    request_tournament = Tournament.query.get(tournament_id)
+    if request_tournament:
+        players = request_tournament.players
+        return jsonify({'tournament': request_tournament,
+                        'players': players}), 200
+    else:
+        return jsonify({'error': 'tournament not found'}), 404
+
+
+# Populate PlayersAndTournament at the tournament start (url to be updated)
+# @app.route('/tournaments/start', methods=['POST'])
+# def start_tournament():
+#     try:
+#         response_data = flask_functions.add_players_to_tournament_db(db=db, db_table=PlayersAndTournaments)
+#         return jsonify(response_data), 201
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
